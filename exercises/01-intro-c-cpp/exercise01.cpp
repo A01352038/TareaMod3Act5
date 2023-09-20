d4c98b1b725b876a6e6befe0d5e0315ff9808546
A01352038 <112148764+A01352038@users.noreply.github.com>
2023-09-20 02:21:47 -0600

    Update exercise01.cpp

--- exercises/01-intro-c-cpp/exercise01.cpp
+++ exercises/01-intro-c-cpp/exercise01.cpp
@@ -1,7 +1,8 @@
 // =================================================================
 //
 // File: exercise01.cpp
-// Author(s):
+// Author(s): Sergio Augusto Macías Corona
+// ID: A01352038
 // Description: This file contains the code to count the number of
 //				even numbers within an array. The time this implementation
 //				takes will be used as the basis to calculate the
@@ -10,6 +11,8 @@
 // =================================================================
 
 #include <iostream>
+#include <stdlib.h>
+#include <stdio.h>
 #include <iomanip>
 #include <chrono>
 #include "utils.h"
@@ -17,15 +20,44 @@
 using namespace std;
 using namespace std::chrono;
 
-// array size
-#define SIZE 1000000000
-
 // implement your code
 
 int main(int argc, char* argv[]) {
-	int *array, result;
+	int *array, result, tamanoarreglo;
+    cout << "Tamano del arreglo ---> "; cin>>tam;
 	// These variables are used to keep track of the execution time.
 	high_resolution_clock::time_point start, end;
+	double timeElapsed;
+
+	array = new int[tamanoarreglo];
+    for(int i = 0; i<tamanoarreglo; i++){
+        int numero;
+        cout<<"Establezca el numero ---> "<<i +1<<": "; cin>>numero;
+        array[i] = numero;
+    }
+	display_array("array", array, tamanoarreglo);
+
+	cout << "Starting...\n";
+	timeElapsed = 0;
+	for (int j = 0; j < tamanoarreglo; j++) {
+		start = high_resolution_clock::now();
+
+		// call the implemented function
+        if(array[j]%2==0){
+          	result = result + array[j];
+    	}
+
+		end = high_resolution_clock::now();
+		timeElapsed += 
+			duration<double, std::milli>(end - start).count();
+	}
+	cout << "result = " << result << "\n";
+	cout << "avg time = " << fixed << setprecision(7) 
+		 << (timeElapsed / N) <<  " ms\n";
+
+	delete [] array;
+	return 0;
+}
 	double timeElapsed;
 
 	array = new int[SIZE];
